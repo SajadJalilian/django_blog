b43264df4a7f477919efd2dd09e733e2d84a1347
Sajad Jalilian <sajadjalilian88@gmail.com>
2020-01-24 12:38:25 +0330

    end of part 5 tutorial

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'Sajad',
-        'title': 'Blog Post 1',
-        'content': 'First blog post',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Rana',
-        'title': 'Blog Post 2',
-        'content': 'First blog post',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
