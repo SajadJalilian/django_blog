6e23650cec00808a93a68bac35032b23067ef0ea
Sajad Jalilian <sajadjalilian88@gmail.com>
2020-06-10 12:16:34 +0430

    fixing user api password security problem

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -13,3 +13,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password', 'email']
+
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    # TODO : fixing security problem with passwords in api
+    #        api returns all passwords
+    # TODO : fixing hashing problen with password creation with api
