8dd3eed2beefc51789600fc96296913cc295927e
Sajad Jalilian <sajadjalilian88@gmail.com>
2020-06-11 20:38:51 +0430

    fixing hashing password in api password creation

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,5 +1,7 @@
 from users.models import Profile
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
 from rest_framework import serializers
 
 
@@ -9,15 +11,19 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ['user', 'image']
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text='Leave empty if no change needed',
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'password', 'email']
+        fields = ('url', 'username', 'email', 'password')
 
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-
-    # TODO : fixing security problem with passwords in api
-    #        api returns all passwords
-    # TODO : fixing hashing problen with password creation with api
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSerializer, self).create(validated_data)
