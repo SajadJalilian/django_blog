a9fc9d7707354e4f4b7732d0b4536c30dd56b354
Sajad Jalilian <sajadjalilian88@gmail.com>
2020-06-29 16:39:20 +0430

    adding models in admin changing blog models order

--- blog/models.py
+++ blog/models.py
@@ -3,20 +3,6 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
-
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
-
-
 class Category(models.Model):
     title = models.CharField(max_length=200)
 
@@ -25,6 +11,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
 
 
 class Comment(models.Model):
